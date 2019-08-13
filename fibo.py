356a428ea7905fdcedbf7f6061deb4f6c8d308b9
Nagammam <51911489+Nagammam@users.noreply.github.com>
2019-08-13 15:47:39 +0530

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -5,20 +5,35 @@ for i in range(n):
     print(a)
     c=a+b
     a=b
-    b=c
-n=int(input("enter a number "))
+    b=cn=int(input("enter a number "))
+10
 a=0
+11
 b=1
+12
 for i in range(n):
+13
     print(a)
+14
     c=a+b
+15
     a=b
+16
     b=c
+17
 n=int(input("enter a number "))
+18
 a=0
+19
 b=1
+20
 for i in range(n):
+21
     print(a)
+22
     c=a+b
+23
     a=b
+24
     b=c
+25
